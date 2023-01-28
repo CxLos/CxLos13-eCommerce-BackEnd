9907e8da1619903905da82bc2bc59cdd5cb50f4c
cxlos <bautistac_29@outlook.com>
2023-01-28 17:46:14 -0600

    first commit

---
+++ models/Category.js
@@ -0,0 +1,53 @@
+//
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Category extends Model {}
+
+Category.init(
+  {
+      id: {
+        type: DataTypes.INTEGER,
+      }, 
+      category_name: {
+        type: DataTypes.STRING
+      },
+      products: {
+        id: {
+          type: DataTypes.INTEGER
+        },
+        product_name: {
+          type: DataTypes.STRING
+        },
+        price: {
+          type: DataTypes.INTEGER
+        },
+        stock: {
+          type: DataTypes.INTEGER
+        },
+        category_id: {
+          type: DataTypes.INTEGER
+        },
+      },
+    // title: {
+    //   type: DataTypes.STRING,
+    //   validate: {
+    //     is: ['[a-z]','i'],        // will only allow letters
+    //     max: 23,                  // only allow values <= 23
+    //     isIn: {
+    //       args: [['en', 'zh']],
+    //       msg: "Must be English or Chinese"
+    //     }
+    //   },
+    // },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'category',
+  }
+);
+
+module.exports = Category;
